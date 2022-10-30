86071e484a29ba56df7e3ad7fd7cfdf7a0727141
윤희찬 <yhc920923@gmail.com>
2022-10-30 17:28:04 +0900

    getMapping, DB setting

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -17,8 +17,8 @@
     <div class="album py-5 bg-light">
       <div class="container">
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-          <div class="col" v-for="i in 12" :key="i">
-            <Card/>
+          <div class="col" v-for="(item, idx) in state.items" :key="idx">
+            <Card :item="item"/>
           </div>
         </div>
       </div>
@@ -26,11 +26,24 @@
   </div>
 </template>
 <script>
+import axios from "axios";
+import {reactive} from "vue";
 import Card from "@/components/Card";
 
 export default {
   name: "Home",
-  components: {Card}
+  components: {Card},
+  setup(){
+    const state = reactive({
+      items:[]
+    })
+
+    axios.get("/api/items").then(({data}) => {
+      state.items = data;
+    })
+
+    return {state}
+  }
 }
 
 </script>
