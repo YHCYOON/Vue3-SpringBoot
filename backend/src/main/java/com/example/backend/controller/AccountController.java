2d71ad28b7a53062616c07903b2ac398897cf80b
윤희찬 <yhc920923@gmail.com>
2022-11-04 19:51:32 +0900

    로그인 보완

--- backend/src/main/java/com/example/backend/controller/AccountController.java
+++ backend/src/main/java/com/example/backend/controller/AccountController.java
@@ -1,15 +1,20 @@
-package com.example.backend.Controller;
+package com.example.backend.controller;
 
 
 import com.example.backend.entity.Member;
 import com.example.backend.repository.MemberRepository;
+import com.example.backend.service.JwtService;
+import com.example.backend.service.JwtServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 
@@ -20,13 +25,21 @@ public class AccountController {
     MemberRepository memberRepository;
 
     @PostMapping("/api/account/login")
-    public int login(
-            @RequestBody Map<String, String> params
-    ) {
+    public ResponseEntity login(@RequestBody Map<String, String> params,
+                                HttpServletResponse res) {
         Member member = memberRepository.findByEmailAndPassword(params.get("email"), params.get("password"));
 
         if(member != null) {
-            return member.getId();
+            JwtService jwtService = new JwtServiceImpl();
+            int id = member.getId();
+            String token = jwtService.getToken("id", id);
+
+            Cookie cookie = new Cookie("token", token);
+            cookie.setHttpOnly(true);
+            cookie.setPath("/");
+
+            res.addCookie(cookie);
+            return new ResponseEntity<>(id, HttpStatus.OK);
         }
         throw new ResponseStatusException(HttpStatus.NOT_FOUND);
     }
--- backend/src/main/java/com/example/backend/controller/ItemController.java
+++ backend/src/main/java/com/example/backend/controller/ItemController.java
@@ -1,4 +1,4 @@
-package com.example.backend.Controller;
+package com.example.backend.controller;
 
 
 import com.example.backend.entity.Item;
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
